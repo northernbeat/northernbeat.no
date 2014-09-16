49bf42e18870b04f74a4e3db84c8c26de2e209e4
Eirik Refsdal <eirikref@gmail.com>
2014-09-16 13:16:11 +0200

    Step One: References on frontpage.

--- wp-content/themes/northernbeat_versjon2/index.php
+++ wp-content/themes/northernbeat_versjon2/index.php
@@ -118,6 +118,101 @@ get_header(); ?>
         <div class="block transport divider">
 			<div class="wrapper wide">
 				<h2><a href="<?php bloginfo("url") ?>/referanser">Referanser</a></h2>
+
+
+
+
+
+
+  <section>
+    	<div class="block main">
+			<div class="wrapper wide">
+				<div class="boxflow">
+            <span class="row clearfix">
+            
+          <?php  $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+			$args = array(
+				'paged'=>$paged,
+				'post_type' => 'referanse',
+				'orderby' => 'date',
+				      );
+
+			query_posts($args);
+			$x = 0;
+            
+		        if ( have_posts() ) : 
+               $post_loop = 0;
+               ?>
+			<?php
+
+			/* Start the Loop */
+
+			while ( have_posts() ) : the_post(); ?>
+            
+            <?php if($post_loop>0 && $post_loop%3 == 0):
+            ?>
+               </span>
+               <span class="row clearfix">
+            <?php endif; 
+
+				/* Include the post format-specific template for the content. If you want to
+
+				 * this in a child theme then include a file called called content-___.php
+
+				 * (where ___ is the post format) and that will be used instead.
+
+				 */ ?>
+
+				<div class="textbox third">
+                
+	<div class="wrapper image">
+		<a href="<?php the_permalink(); ?>"><?php $post_id = get_the_ID(); echo northernbeat_post_auto_thumbnail($post_id); ?></a>
+	</div>
+	<div class="wrapper text">
+		<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+		<p><?php the_excerpt(); ?></p>
+	</div>
+				</div>
+             <?php 
+			 $post_loop++;
+			endwhile;
+
+			//northernbeat_content_nav( 'nav-below' );
+            if ( function_exists( 'wp_paginate' ) ) {
+			wp_paginate();
+    		}
+    		else{
+	           northernbeat_content_nav( 'nav-below' );
+	         }
+
+        ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'content', 'none' ); ?>
+
+		<?php endif; ?>
+
+
+	</span>
+				</div>
+			</div>
+    	</div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+                <!--
                 <div class="aktuelt_forside">
                 <?php 
 					$teller = 0;
@@ -136,7 +231,7 @@ get_header(); ?>
 						</div>
 					<?php endwhile;  ?>
                         
-					</div><!-- END loop -->
+					</div>
                     
                   <?php if ( is_active_sidebar( 'sidebar-recent' ) ) : ?>
     
@@ -144,10 +239,10 @@ get_header(); ?>
 
 						<?php dynamic_sidebar( 'sidebar-recent' ); ?>
 
-						</div><!-- #sidebar footer -->
+						</div>
 
 					<?php endif; ?>
-
+                    -->
    	        </div>
     	</div>
       </section>
