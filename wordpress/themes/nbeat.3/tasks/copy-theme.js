510db60d872e9a70195531f2860fda6a7c6a52a5
Eirik Refsdal <eirikref@gmail.com>
2015-06-26 11:40:11 +0200

    Better build process

--- wordpress/themes/nbeat.3/tasks/copy-theme.js
+++ wordpress/themes/nbeat.3/tasks/copy-theme.js
@@ -2,7 +2,19 @@ module.exports = function(gulp, plugins)
 {
     return function()
     {
-        return gulp.src("src/theme/**/*").
-            pipe(gulp.dest("build/NorthernBeat3"));
+        gulp.src("src/templates/**/*")
+            .pipe(gulp.dest("build/NorthernBeat3"));
+
+        gulp.src(["src/lib/**/*", "!src/lib/functions.php"])
+            .pipe(gulp.dest("build/NorthernBeat3/lib"));
+
+        gulp.src("src/lib/functions.php")
+            .pipe(gulp.dest("build/NorthernBeat3"));
+
+        gulp.src("src/views/**/*")
+            .pipe(gulp.dest("build/NorthernBeat3/views"));
+
+        gulp.src("vendor/**/*")
+            .pipe(gulp.dest("build/NorthernBeat3/vendor"));
     };
 };
