773d8026fa8a1b09932d092ca01d05235b15ef4f
Eirik Refsdal <eirikref@gmail.com>
2015-09-16 15:42:11 +0200

    Set numberposts for employee archive

--- wordpress/themes/nbeat.3/src/templates/archive-employee.php
+++ wordpress/themes/nbeat.3/src/templates/archive-employee.php
@@ -1,6 +1,7 @@
 <?php
 
-$args            = array("post_type" => "employee");
+$args            = array("post_type"   => "employee",
+                         "numberposts" => 50);
 $data            = Timber::get_context();
 $data["posts"]   = Timber::get_posts($args, "\\NorthernBeat\\Theme\\Employee");
 $data["classes"] = "list-of-three";
