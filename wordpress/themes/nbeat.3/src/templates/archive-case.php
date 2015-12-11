e582f0743e5cec777b1eaf48097786bbab5f1d28
Eirik Refsdal <eirikref@gmail.com>
2015-12-11 08:13:22 +0100

    Fetch 50 cases

--- wordpress/themes/nbeat.3/src/templates/archive-case.php
+++ wordpress/themes/nbeat.3/src/templates/archive-case.php
@@ -1,6 +1,7 @@
 <?php
 
-$args          = array("post_type" => "case");
+$args          = array("post_type"   => "case",
+                       "numberposts" => 50);
 $data          = Timber::get_context();
 $data["posts"] = Timber::get_posts($args, "\\NorthernBeat\\Theme\\Showcase");
 
